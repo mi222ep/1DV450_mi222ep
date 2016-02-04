b8f6e924804af569898c9a8b09283dad6da715e6
Matilda laptop <ingman.m@gmail.com>
2016-02-04 11:45:00 +0100

    Uppdating seeds file

--- posapp/db/seeds.rb
+++ posapp/db/seeds.rb
@@ -5,3 +5,17 @@
 #
 #   cities = City.create([{ name: 'Chicago' }, { name: 'Copenhagen' }])
 #   Mayor.create(name: 'Emanuel', city: cities.first)
+User.create(name: "john_Snow", password: "4thewall")
+User.create(name: "albin", password: "123456")
+
+a = Application.create(name:"test1", api_key:"123abc",about:"About example application")
+User.find(1).applications << a
+
+a = Application.create(name:"test2", api_key:"123abc",about:"About example application")
+User.find(1).applications << a
+
+a = Application.create(name:"test3", api_key:"123abc",about:"About example application")
+User.find(1).applications << a
+
+a = Application.create(name:"test4", api_key:"123abc",about:"About example application")
+User.find(2).applications << a
