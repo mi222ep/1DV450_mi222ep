7086a327908eef3b8345c5389f5398aab03b48db
Matilda laptop <ingman.m@gmail.com>
2016-02-21 18:19:41 +0100

    login now working, I think

--- posapp/config/routes.rb
+++ posapp/config/routes.rb
@@ -2,5 +2,5 @@ Rails.application.routes.draw do
   get 'users/new' => 'users#new'
   get 'users/' => 'users#index'
   get '/login' => 'sessions#new'
-  post '/login' => 'sessions#api_auth'
+  post '/login' => 'sessions#create'
 end
