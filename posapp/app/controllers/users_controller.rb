aa004e13d10a6d39d1922d3b342675c8ca30e9f1
Matilda laptop <ingman.m@gmail.com>
2016-02-23 18:25:50 +0100

    Flash messages on login screen

--- posapp/app/controllers/users_controller.rb
+++ posapp/app/controllers/users_controller.rb
@@ -11,6 +11,7 @@ class UsersController < ApplicationController
       @user = User.new(user_params)
 
       if @user.save
+        flash[:info] = "Registration sucessful, you can now log in"
         redirect_to login_path
       else
         render :action => "new"
