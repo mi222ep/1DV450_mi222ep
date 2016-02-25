cfc0f312fccbb8e75c1d800ef361e3283d3e43a8
Matilda laptop <ingman.m@gmail.com>
2016-02-25 15:32:31 +0100

    validation in models

--- posapp/app/models/apikey.rb
+++ posapp/app/models/apikey.rb
@@ -1,4 +1,14 @@
 class Apikey < ActiveRecord::Base
+    validates :name,
+              :presence => {:message =>"Name can't be empty"},
+              :length => {:minimum =>6, :message => "Username must be at least 6 keys" },
+              :length => {:maximum => 20, :message => "Username cannot be longer than 20 characters"}
+
+    validates :about,
+              :presence => {:message =>"About can't be empty"},
+              :length => {:minimum =>6, :message => "About must be at least 6 keys" },
+              :length => {:maximum => 200, :message => "Username cannot be longer than 200 characters"}
+
   belongs_to :user
 
 end
--- posapp/app/models/user.rb
+++ posapp/app/models/user.rb
@@ -2,7 +2,8 @@ class User < ActiveRecord::Base
   has_secure_password
   validates :name,
             :presence => {:message =>"Name can't be empty"},
-            :length => {:minimum =>6, :message => "Username must be at least 6 keys" }
+            :length => {:minimum =>6, :message => "Username must be at least 6 keys" },
+            :length => {:maximum => 20, :message => "Username cannot be longer than 20 characters"}
 
 
   has_many :applications
