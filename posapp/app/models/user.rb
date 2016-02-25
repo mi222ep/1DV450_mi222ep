0b3ef18f183801ab4c71c42dcb923327f47ff7f1
Matilda laptop <ingman.m@gmail.com>
2016-02-25 15:26:30 +0100

    validating user

--- posapp/app/models/user.rb
+++ posapp/app/models/user.rb
@@ -1,7 +1,9 @@
 class User < ActiveRecord::Base
   has_secure_password
   validates :name,
-            :presence => {:message =>"Du måste ange ett namn"}
+            :presence => {:message =>"Name can't be empty"},
+            :length => {:minimum =>6, :message => "Username must be at least 6 keys" }
+
 
   has_many :applications
 end
