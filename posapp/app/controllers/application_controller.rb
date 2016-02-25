883daf2e8d30e4c2e1b312d3999faf56553754b6
Matilda laptop <ingman.m@gmail.com>
2016-02-25 15:13:43 +0100

    Menu added

--- posapp/app/controllers/application_controller.rb
+++ posapp/app/controllers/application_controller.rb
@@ -10,16 +10,20 @@ class ApplicationController < ActionController::Base
     @current_user ||= User.find(session[:user_id]) if session[:user_id]
   end
 
+  def is_admin
+    @current_user.id == 1
+  end
+
   def require_login
     if current_user.nil? then
       flash[:info] = "You must be logged in to visit that page"
       redirect_to login_path
     end
   end
-  def require_admin_access
 
-    if @current_user.id == 1
-     # flash[:info] = "Logged in as admin"
+  def require_admin_access
+    if @current_user.id != 1
+     redirect_to keys_path
     end
   end
 end
