7b071500145bcbeb4cf8fe175fb6434730f72f2e
Matilda laptop <ingman.m@gmail.com>
2016-02-29 16:02:36 +0100

    Preparing database

--- posapp/db/schema.rb
+++ posapp/db/schema.rb
@@ -11,7 +11,7 @@
 #
 # It's strongly recommended that you check this file into your version control system.
 
-ActiveRecord::Schema.define(version: 20160223144854) do
+ActiveRecord::Schema.define(version: 20160229145406) do
 
   create_table "apikeys", force: true do |t|
     t.string   "name"
@@ -23,6 +23,38 @@ ActiveRecord::Schema.define(version: 20160223144854) do
   end
 
   add_index "apikeys", ["user_id"], name: "index_apikeys_on_user_id"
+
+  create_table "creators", force: true do |t|
+    t.datetime "created_at"
+    t.datetime "updated_at"
+  end
+
+  create_table "events", force: true do |t|
+    t.datetime "created_at"
+    t.datetime "updated_at"
+  end
+
+  create_table "events_tags", id: false, force: true do |t|
+    t.integer "event_id"
+    t.integer "tag_id"
+  end
+
+  add_index "events_tags", ["event_id", "tag_id"], name: "index_events_tags_on_event_id_and_tag_id"
+
+  create_table "positions", force: true do |t|
+    t.datetime "created_at"
+    t.datetime "updated_at"
+  end
+
+  create_table "tag_types", force: true do |t|
+    t.datetime "created_at"
+    t.datetime "updated_at"
+  end
+
+  create_table "tags", force: true do |t|
+    t.datetime "created_at"
+    t.datetime "updated_at"
+  end
 
   create_table "users", force: true do |t|
     t.string   "name",            null: false
