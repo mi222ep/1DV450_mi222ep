181cd812001ce4c6794f3df238ed0588e0714870
Matilda laptop <ingman.m@gmail.com>
2016-03-05 13:33:06 +0100

    Fiexed wrong relation as of issue #7

--- posapp/app/models/user.rb
+++ posapp/app/models/user.rb
@@ -4,5 +4,5 @@ class User < ActiveRecord::Base
             :presence => {:message =>"Name can't be empty"},
             :length => {:minimum =>6, :maximum => 20, :message => "Username must be between 6 and 20 characters" }
 
-  has_many :applications
+  has_many :apikeys
 end
