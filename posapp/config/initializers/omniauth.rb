03ecd99ed3f9684b1ef7dd0dd830c6700d4b42d5
Matilda laptop <ingman.m@gmail.com>
2016-03-06 13:19:40 +0100

    Omniauth twitter enabled

---
+++ posapp/config/initializers/omniauth.rb
@@ -0,0 +1,3 @@
+Rails.application.config.middleware.use OmniAuth::Builder do
+  provider :twitter, "CwJkzeAGWFbh06BA4oYtjI2PH", "EOgXG9IduNxP3dC8mQDe2KnI4uOIKkgxRpmKomEGrje8NQyrIg"
+end
--- posapp/config/routes.rb
+++ posapp/config/routes.rb
@@ -17,4 +17,5 @@ Rails.application.routes.draw do
 
   get '/admin' => 'admin#show'
   delete '/admin/:id/delete' => 'admin#delete'
+
 end
