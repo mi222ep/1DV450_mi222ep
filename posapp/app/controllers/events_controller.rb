8ea3500e667146ba98d41d9e663db7ae2f21e785
Matilda laptop <ingman.m@gmail.com>
2016-03-11 18:55:49 +0100

    Pushing what is to be rendered to controlls

--- posapp/app/controllers/events_controller.rb
+++ posapp/app/controllers/events_controller.rb
@@ -8,12 +8,25 @@ class EventsController < ApplicationController
 
   def get_single_event
     @event = Event.find_by_id(params['eventID'])
-    render :json =>@event
+    toClient = []
+    toClient.push("posapp" =>api_header)
+    toClient.push("data" =>@event)
+    #@a.push("footer" => api_footer)
+    render :json =>toClient
   end
 
   def get_offset_and_limit
-    @event =Event.select('events.name, about, longitude, event_time, latitude, creators.name AS "created-by"').joins(:position, :creator).order(event_time: :asc).all
-    render :json =>@event.limit(params['limit']).offset(params['offset'])
+    a = []
+    @event =Event.select('events.name, about, longitude, event_time, latitude, creators.name AS "created-by"').joins(:position, :creator).order(event_time: :asc).limit(params['limit']).offset(params['offset'])
+    a.push(@event)
+    render :json =>a
+  end
+  def api_header
+    return "header: api-header"
+
+  end
+  def api_footer
+    return "footer: api-footer"
   end
 
 end
