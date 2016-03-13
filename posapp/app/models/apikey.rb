c29a5e800dc0146c9d1a6226d386f2740b95aab3
Matilda laptop <ingman.m@gmail.com>
2016-03-13 18:23:39 +0100

    Change max length of apikey name

--- posapp/app/models/apikey.rb
+++ posapp/app/models/apikey.rb
@@ -1,7 +1,7 @@
 class Apikey < ActiveRecord::Base
     validates :name,
               :presence => {:message =>"Name can't be empty"},
-              :length => {:minimum =>6, :maximum => 20, :message => "Name must be between 6 and 20 characters long"}
+              :length => {:minimum =>6, :maximum => 50, :message => "Name must be between 6 and 20 characters long"}
 
     validates :about,
               :presence => {:message =>"About can't be empty"},
