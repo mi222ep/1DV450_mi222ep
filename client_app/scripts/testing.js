f42935dfc83734e79e5f4a51cba0940c805d8f1c
Matilda laptop <ingman.m@gmail.com>
2016-03-13 22:02:44 +0100

    Objects are returning, hooray!

--- client_app/scripts/testing.js
+++ client_app/scripts/testing.js
@@ -1,14 +1,16 @@
 var app = angular.module('myApp', []);
 app.controller('myCtrl', function($scope, $http) {
     var config = {headers:  {
-        'api_key' : "123",
+        'apikey' : "123",
         'crossDomain' : 'true',
         'Access-Control-Allow-Origin' :"*",
         'Access-Control-Allow-Methods' : 'POST, GET',
         'Access-Control-Allow-Headers': '*'
     }
     };
-    $http.get('http://localhost:3000/events/3', config).sucess = function($data){
-        console.debug($data);
-    }
+    $http.get('http://localhost:3000/events/', config).success(function($data){
+        console.log($data);
+        console.log("Sucess");
+    })
+    console.log("Klart");
 });
